f68ec09d5cffa03ef2b73fc3d833bf583672112b
Kyan He <kyan.ql.he@gmail.com>
2011-05-06 01:36:50 +0800

    update client

--- client.py
+++ client.py
@@ -6,5 +6,5 @@ import struct
 SERVER = ("127.0.0.1", 20504)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-data = struct.pack('>III6s', 1, 1, 1, 'abcdef')
+data = struct.pack('>IIII6sI', 1, 1, 165536, 3, 'abcdef', 0)
 s.sendto(data, SERVER)
