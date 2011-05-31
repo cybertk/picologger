d3807d65b13b67ef223ae139926350c80cce89d1
Kyan He <kyan.ql.he@gmail.com>
2011-05-31 18:44:41 +0800

    jsyslog: Update syslog parser. 	- Parse NILVALUE SD. 	- Parse log without message. 	- Add related tests. 	- Add encode(), which will generate RFC5424 log string. 	- Add setter&getter.

--- jsyslogd/src/com/github/picologger/syslog/Demo.java
+++ jsyslogd/src/com/github/picologger/syslog/Demo.java
@@ -5,47 +5,67 @@ import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Demo {
-
+public class Demo
+{
+    
     private static String record = "<165>1 2003-08-24T05:14:15.000003-07:00 192.0.2.1 myproc 8710 - - %% It's time to make the do-nuts.";
-
+    
     private static String recordWithSd = "<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 [exampleSDID@32473 iut=\"3\" eventSource=\"Application\" eventID=\"1111\"] [exampleSDID@32473 iut=\"3\" eventSource=\"Application\" eventID=\"1011\"] BOMAn application event log entry...";
-
+    
     /**
      * @param args
      */
-    public static void main(String[] args) {
-
+    public static void main(String[] args)
+    {
+        
         // TODO Auto-generated method stub
-
-        Syslog l = new Syslog(recordWithSd);
+        
+        Syslog l = new Syslog(record);
         System.out.println(l);
-
+        
+        l = new Syslog(recordWithSd);
+        System.out.println(l);
+        
+        Syslog l1 = new Syslog();
+        String raw = l1.encode();
+        System.out.println(raw);
+        
+        l = new Syslog(raw);
+        System.out.println(l);
+        
         ServerSocket s = null;
-        try {
+        try
+        {
             s = new ServerSocket(58858);
-        } catch (IOException e) {
+        }
+        catch (IOException e)
+        {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
-        while (true) {
+        
+        while (true)
+        {
             Socket c;
-            try {
+            try
+            {
                 c = s.accept();
-
+                
                 InputStream is = c.getInputStream();
-
+                
                 byte[] bytes = new byte[4096];
                 int bytesRead = is.read(bytes);
-
-                if (bytesRead == -1) {
+                
+                if (bytesRead == -1)
+                {
                     break;
                 }
-  
+                
                 Syslog log = new Syslog(new String(bytes, 0, bytesRead));
                 System.out.println(log);
-            } catch (IOException e) {
+            }
+            catch (IOException e)
+            {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
--- jsyslogd/src/com/github/picologger/syslog/Syslog.java
+++ jsyslogd/src/com/github/picologger/syslog/Syslog.java
@@ -32,6 +32,106 @@ public class Syslog
     // Indicates this is RFC3164 or RFC5424
     private boolean bsd;
     
+    public int getFacility()
+    {
+        return facility;
+    }
+    
+    public void setFacility(int facility)
+    {
+        this.facility = facility;
+    }
+    
+    public int getSeverity()
+    {
+        return severity;
+    }
+    
+    public void setSeverity(int severity)
+    {
+        this.severity = severity;
+    }
+    
+    public int getVersion()
+    {
+        return version;
+    }
+    
+    public void setVersion(int version)
+    {
+        this.version = version;
+    }
+    
+    public String getTimestamp()
+    {
+        return timestamp;
+    }
+    
+    public void setTimestamp(String timestamp)
+    {
+        this.timestamp = timestamp;
+    }
+    
+    public String getHostname()
+    {
+        return hostname;
+    }
+    
+    public void setHostname(String hostname)
+    {
+        this.hostname = hostname;
+    }
+    
+    public String getAppname()
+    {
+        return appname;
+    }
+    
+    public void setAppname(String appname)
+    {
+        this.appname = appname;
+    }
+    
+    public String getProcid()
+    {
+        return procid;
+    }
+    
+    public void setProcid(String procid)
+    {
+        this.procid = procid;
+    }
+    
+    public String getMsgid()
+    {
+        return msgid;
+    }
+    
+    public void setMsgid(String msgid)
+    {
+        this.msgid = msgid;
+    }
+    
+    public String getSd()
+    {
+        return sd;
+    }
+    
+    public void setSd(String sd)
+    {
+        this.sd = sd;
+    }
+    
+    public String getMsg()
+    {
+        return msg;
+    }
+    
+    public void setMsg(String msg)
+    {
+        this.msg = msg;
+    }
+    
     public Syslog(String record)
     {
         decode(record);
@@ -39,10 +139,47 @@ public class Syslog
     
     public Syslog()
     {
+        // Local use 0 (local0).
+        facility = 16;
+        
+        // Debug.
+        severity = 7;
+        
+        // RFC Specified.
+        version = 1;
+        timestamp = "-";
+        hostname = "-";
+        appname = "-";
+        procid = "-";
+        msgid = "-";
+        sd = "-";
+        msg = "";
+        
     }
     
-    public void encode() {
+    public String encode()
+    {
+        String str = "";
         
+        // Generates PRI.
+        int pri = (facility >> 3) + severity;
+        str += "<" + pri + ">";
+        
+        // Generates version.
+        str += version + " ";
+        str += timestamp + " ";
+        str += hostname + " ";
+        str += appname + " ";
+        str += procid + " ";
+        str += msgid + " ";
+        str += sd;
+        
+        if (!msg.isEmpty())
+        {
+            str += " " + msg;
+        }
+        
+        return str;
     }
     
     @Override
@@ -63,7 +200,7 @@ public class Syslog
         
         return str;
     }
-
+    
     private void decode(String record) throws IllegalArgumentException
     {
         int pos0 = 0;
@@ -79,14 +216,13 @@ public class Syslog
         // Parse Header.
         
         // Parse facility and severity.
-        int pri = Integer.decode(record.substring(1,pos));
+        int pri = Integer.decode(record.substring(1, pos));
         facility = pri >> 3;
         severity = pri & 0x7;
         
         // Parse Version.
         ++pos;
         version = record.charAt(pos) - 0x30;
-            
         
         String[] token = record.split(" +", 7);
         
@@ -97,26 +233,42 @@ public class Syslog
         msgid = token[5];
         
         // Parse SD
-        while (true)
+        if (token[6].charAt(0) == '[')
         {
-            pos0 = token[6].indexOf(']', pos0);
-            
-            if (pos0 == -1)
+            while (true)
             {
-                break;
-            }
-            
-            ++pos0;
-            
-            // Record the index.
-            if (token[6].charAt(pos0 - 2) != '\\')
-            {
-                // Make sure it's not a escaped "]".
-                pos = pos0;
+                pos0 = token[6].indexOf(']', pos0);
+                if (pos0 == -1)
+                {
+                    break;
+                }
+                
+                ++pos0;
+                
+                // Record the index.
+                if (token[6].charAt(pos0 - 2) != '\\')
+                {
+                    // Make sure it's not a escaped "]".
+                    pos = pos0;
+                }
             }
         }
-        
+        else
+        {
+            // NILVAULE, "-".
+            
+            pos = 1;
+        }
         sd = token[6].substring(0, pos);
-        msg = token[6].substring(pos + 1);
+        
+        // Parse message.
+        if (pos > token[6].length())
+        {
+            msg = token[6].substring(pos + 1);
+        }
+        else
+        {
+            msg = "";
+        }
     }
 }
